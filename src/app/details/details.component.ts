39a4ccc592605784fd55dd3bf6bfd5636ea1efab
ftijan <41797259+ftijan@users.noreply.github.com>
2024-11-03 00:52:26 +0000

    add completed exercise files (#1)
    * add created generated project files
    * assets, initial comps, styles
    * more comps
    * ngFor
    * use service to list data
    * details page, routing
    * reactive forms
    * search filter
    * fetch and async service call with local json server

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+
+  housingLocationId = -1;
+  housingLocation: HousingLocation | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id'])
+    // Switch from local data to async service call
+    //this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+    this.housingService.getHousingLocationById(this.housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+    });
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  housingService: HousingService = inject(HousingService);
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+  
+  constructor() {
+    // Switch from local data to async service call
+    //  this.housingLocationList = this.housingService.getAllHousingLocations();
+    //  this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = this.housingLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => 
+      housingLocation?.city.toLocaleLowerCase().includes(text.toLowerCase()),
+    );
+  }
+}
