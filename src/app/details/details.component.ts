a0e2678589601142b05b1041f7070f38f65587e6
Filip Tijan <ftijan@gmail.com>
2024-11-03 00:27:49 +0000

    reactive forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -17,8 +18,22 @@ export class DetailsComponent {
   housingLocationId = -1;
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+  
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id'])
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
